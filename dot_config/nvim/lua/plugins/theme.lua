8996505c1fbad155fad8384f97e899548a83739c
Ananth Pullur <106897936+Uncool-dudes@users.noreply.github.com>
2025-06-30 11:17:05 +0530

    Added some dotfiles

---
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,13 @@
+return {
+  { 'nyoom-engineering/oxocarbon.nvim' },
+  { 'datsfilipe/min-theme.nvim' },
+  { 'savq/melange-nvim' },
+  { 'tanvirtin/monokai.nvim' },
+  { 'killitar/obscure.nvim' },
+  {
+    'LazyVim/LazyVim',
+    opts = {
+      colorscheme = 'oxocarbon',
+    },
+  },
+}
